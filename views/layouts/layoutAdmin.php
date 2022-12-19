8acfb51b2f3ea0c963aed701e3d1d314da911066
Esteban Redón <esteban1.redon2@gmail.com>
2022-12-19 17:18:03 -0300

    Se agregó un ícono para la aplicación, se cambió el botón de registrarse de la página principal

--- views/layouts/layoutAdmin.php
+++ views/layouts/layoutAdmin.php
@@ -11,6 +11,8 @@
 	<meta name="robots" content="noindex">
 	<meta name="robots" content="nofollow">
   <title><?= $title; ?> | Equizzy</title>
+  <!-- App Icon -->
+	<link rel="shortcut icon" href="/build/img/EquizzyAppIcon.svg" type="image/x-icon">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Custom CSS -->
